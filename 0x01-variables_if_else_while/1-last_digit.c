92d40a97d6b2d1bb5a1cd4a6230df7de7e954e9f
SyndeeRain <eko1994precious@gmail.com>
2022-12-09 03:30:21 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Determines either greater than 5, is than 6, or is 0
+ * main - Determines either greater than 5, is than 6, or is 0.
  *
  * Return: Always 0 (Success)
  */
